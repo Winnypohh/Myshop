c947b0404a888646386881deea2dbd75642248de
Winnypohh <maik523@hotmail.com>
2022-03-25 21:52:37 +0000

    Cant get my remove button to work

--- bag/contexts.py
+++ bag/contexts.py
@@ -30,7 +30,7 @@ def bag_contents(request):
                     'quantity': quantity,
                     'product': product,
                     'size': size,
-                }) 
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
