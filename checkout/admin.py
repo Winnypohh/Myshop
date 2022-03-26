f7add3f9c44d31f4ffa915e3ffc0ba478034b915
Winnypohh <maik523@hotmail.com>
2022-03-26 18:45:14 +0000

    error in checkout page

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
+
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
