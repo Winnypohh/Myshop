ecce45bb9a9dd9f1adcbb8d595b1e76cf2cf8660
Winnypohh <maik523@hotmail.com>
2022-04-09 17:13:37 +0000

    errror in checkout

--- checkout/static/checkout/css/js/stripe_elements.js
+++ checkout/static/checkout/css/js/stripe_elements.js
@@ -1,14 +1,14 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-    
+
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
+var stripe = stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
